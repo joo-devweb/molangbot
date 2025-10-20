7e406460784448d5d53b6e292a389ad861b7883b
nathan <jojorevanaldi@gmail.com>
2025-10-20 13:24:27 +0700

    Update WhatsApp numbers and API key in config

--- config.ts
+++ config.ts
@@ -1,14 +1,14 @@
 import { Config } from './lib/types';
 
 export const config: Config = {
-  ownerNumber: ["6289526974458"], // Ganti dengan nomor WhatsApp Anda (format 62)
+  ownerNumber: ["your numb"], // Ganti dengan nomor WhatsApp Anda (format 62)
   
   botName: "Molang Bot",
   prefix: ".",
   
-  pairingNumber: "6289526974458", // Ganti dengan nomor yang sama untuk pairing
+  pairingNumber: "pair numb", // Ganti dengan nomor yang sama untuk pairing
   
   sessionName: "molangSessions",
   menuGifUrl: "https://qu.ax/cUFXD.mp4",
-  apiKey: "Btz-mamijojo"
+  apiKey: "your betabotz key"
 };
